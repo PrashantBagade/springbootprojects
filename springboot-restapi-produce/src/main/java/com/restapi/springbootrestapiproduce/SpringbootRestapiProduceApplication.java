99f9cd99dd2773917c0a83fadaea4a07f232dc86
Mahesh Bhoyar <mmbhoyar54@gmail.com>
2018-10-12 22:27:08 +0530

    firstcommit

---
+++ springboot-restapi-produce/src/main/java/com/restapi/springbootrestapiproduce/SpringbootRestapiProduceApplication.java
@@ -0,0 +1,12 @@
+package com.restapi.springbootrestapiproduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiProduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiProduceApplication.class, args);
+	}
+}
